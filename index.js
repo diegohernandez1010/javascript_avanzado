3e40a79cb3b71c6d22ad1c5ae1f88d934ee68190
Diego Hernandez <diegoalejandro061292@gmail.com>
2023-02-23 21:28:16 -0500

    paquete node chalk

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-console.log("hola mundo");
-let aleatorio =[];
 
-for (let i=0; i<10; i++){
-  aleatorio.push(Math.floor(Math.random()*100));
-}
-console.log(aleatorio);
+// import chalk from 'chalk';
+// console.log("hola mundo");
+
+// let aleatorio =[];
+
+// for (let i=0; i<10; i++){
+//   aleatorio.push(Math.floor(Math.random()*100));
+// }
+// console.log(aleatorio);
+
+// import chalk from 'chalk';
+
+// const error = chalk.bold.red;
+// const warning = chalk.hex('#FFA500'); // Orange color
+
+// console.log(error('Error!'));
+// console.log(warning('Warning!'));
+
+// async function chalk(){
+// return(awaitimport("chalk")).default;
+
+// }
+// async function main(){
+//   console.log((awaitchalk()).gray(">",...comannds));
+// }
+
+const chalk = require ('chalk');
+console.log(chalk.blue('hello'));
